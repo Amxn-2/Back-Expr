c61b63cf2c23684c34143a62f0e3c71a3eabf871
Amxn-2 <amansharmaa1205@gmail.com>
2024-01-30 20:30:44 +0530

    Middleware

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (request, response) => {
-    // res.send('Hello Worls!')
-    console.log('PING Received')
-    response.json({message: 'PING Received'});
-})
+const logMiddleware = (req, res, next) => {
+    console.log(`Request received at ${new Date()}`);
+    res.send('Hello');
+    next();
+};
+
+app.use('/home', logMiddleware);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
